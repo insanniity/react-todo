e2f29bd483ccae3dd0fc9d1189654834f7be07ef
Cleydson <cleydson.cg@live.com>
2024-07-02 20:12:35 -0300

    Project Start

---
+++ src/main/java/dev/insannity/react_todo/ReactTodoApplication.java
@@ -0,0 +1,13 @@
+package dev.insannity.react_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/insannity/react_todo/ReactTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.insannity.react_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
